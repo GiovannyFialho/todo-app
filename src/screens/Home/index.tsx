9f3681035c831f04d3fdd79fa303ad77a1e70d3a
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-02-12 16:56:27 -0300

    criacao da tela inicial, sem tarefas

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,81 @@
+import {
+  Image,
+  Keyboard,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+
+import { colors } from "../../theme/colors";
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <View style={styles.containerBanner}>
+          <Image
+            source={require("../../../assets/todo-logo.png")}
+            style={styles.banner}
+          />
+        </View>
+
+        <View style={styles.content}>
+          <View style={styles.form}>
+            <TextInput
+              style={styles.input}
+              placeholder="Adicione uma nova tarefa"
+              placeholderTextColor={colors["gray-300"]}
+            />
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>+</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.headerList}>
+            <View style={styles.headerListItem}>
+              <Text
+                style={[styles.headerListItemTitle, { color: colors.blue }]}
+              >
+                Criadas
+              </Text>
+
+              <View style={styles.headerListItemCountContainer}>
+                <Text style={styles.headerListItemCountItem}>0</Text>
+              </View>
+            </View>
+
+            <View style={styles.headerListItem}>
+              <Text
+                style={[styles.headerListItemTitle, { color: colors.purple }]}
+              >
+                Concluídas
+              </Text>
+
+              <View style={styles.headerListItemCountContainer}>
+                <Text style={styles.headerListItemCountItem}>0</Text>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.noneTask}>
+            <Image
+              source={require("../../../assets/clipboard.png")}
+              style={styles.clipboard}
+            />
+
+            <Text style={styles.noneTaskText}>
+              <Text style={styles.noneTaskTextBold}>
+                Você ainda não tem tarefas cadastradas
+              </Text>{" "}
+              Crie tarefas e organize seus itens a fazer
+            </Text>
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,102 @@
+import { StyleSheet } from "react-native";
+
+import { colors } from "../../theme/colors";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors["gray-700"],
+  },
+  containerBanner: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 80,
+    marginBottom: 70,
+  },
+  banner: {
+    width: 110,
+    height: 32,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: colors["gray-600"],
+    paddingHorizontal: 24,
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    gap: 5,
+    marginTop: -30,
+    marginBottom: 32,
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: colors["gray-500"],
+    borderRadius: 5,
+    color: "#fff",
+    padding: 16,
+    fontSize: 16,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: colors["blue-dark"],
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 24,
+  },
+  headerList: {
+    width: "100%",
+    flexDirection: "row",
+    gap: 10,
+    justifyContent: "space-between",
+  },
+  headerListItem: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  headerListItemTitle: {
+    fontSize: 14,
+    fontWeight: "700",
+  },
+  headerListItemCountContainer: {
+    width: "auto",
+    height: "auto",
+    backgroundColor: colors["gray-400"],
+    borderRadius: 999,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 2,
+    paddingHorizontal: 8,
+  },
+  headerListItemCountItem: {
+    fontSize: 12,
+    fontWeight: "700",
+    color: colors["gray-200"],
+  },
+  noneTask: {
+    alignItems: "center",
+    gap: 16,
+    marginTop: 20,
+    paddingVertical: 48,
+    paddingHorizontal: 20,
+    borderTopWidth: 1,
+    borderTopColor: "#333",
+  },
+  clipboard: {
+    width: 56,
+    height: 56,
+  },
+  noneTaskText: {
+    fontSize: 14,
+    color: colors["gray-400"],
+  },
+  noneTaskTextBold: {
+    fontWeight: "700",
+  },
+});
